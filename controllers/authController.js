ab5caee7a71b936d8a88b4437b2a02e6e1a07ea5
bunmigrey <bunmigrey@icloud.com>
2023-08-20 13:40:44 +0100

    timestamp adjusted

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,15 +17,20 @@ module.exports.query_get = async (req, res) => {
     const { date } = req.body;
 
     if (!date) {
-      return res.status(400).json({ error: 'Date is required in the request body.' });
+        return res.status(400).json({ error: 'Date is required in the request body.' });
     }
 
     try {
-        const docs = await User.find({ createdAt: { $gt: new Date(date) } }).exec();
-        res.json(docs);
-        
+       
+
+        var timeStamp = new Date(date * 1000).toISOString();
+
+        const docs = await User.find({ createdAt: { $gt: new Date(timeStamp) } }).exec();
+
+        res.json({ timeStamp, Docs: docs });
+
     } catch (err) {
-        throw new Error(`Error occured, ${err}`);
+        throw Error(`Error occured, ${err}`);
     }
 
 }
